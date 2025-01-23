70d4d0e044f4bbc551643ab5923e0636efc7a501
Travis F. <nowhererx7@yahoo.com>
2025-01-22 22:04:18 -0700

    Loads values as needed
    Also includes checks for supported values

--- nutwinsvc/RegistryConfigurationProvider.cs
+++ nutwinsvc/RegistryConfigurationProvider.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Win32;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.Versioning;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace NutWinSvc;
 
@@ -20,34 +23,51 @@ internal class RegistryConfigurationProvider : ConfigurationProvider
         prefix = Path.GetFileName(Path.TrimEndingDirectorySeparator(source.Path));
     }
 
-    private void ProcessValues(RegistryKey key, string prefix)
+    private static bool CanHandle(RegistryValueKind valueKind)
     {
+        switch (valueKind)
+        {
+            case RegistryValueKind.DWord:
+            case RegistryValueKind.ExpandString:
+            case RegistryValueKind.QWord:
+            case RegistryValueKind.String:
+                return true;
+            //case RegistryValueKind.None:
+            //case RegistryValueKind.Unknown:
+            //case RegistryValueKind.Binary:
+            //case RegistryValueKind.MultiString:
+            default:
+                return false;
+        }
+    }
+
+    private static void ProcessValues(RegistryKey? key, string prefix, List<string> results)
+    {
+        if (key == null) return;
         foreach (var name in key.GetValueNames())
         {
             try
             {
-                var value = key.GetValue(name);
-                if (Convert.ChangeType(value, typeof(string)) is string str)
-                    Data.Add(prefix + ConfigurationPath.KeyDelimiter + name, str);
+                if (CanHandle(key.GetValueKind(name)))
+                    results.Add(prefix + ConfigurationPath.KeyDelimiter + name);
             }
             catch { }
         }
     }
 
-    private void ProcessKey(RegistryKey key, string prefix)
+    private static void ProcessKey(RegistryKey? key, string prefix, List<string> results)
     {
-        ProcessValues(key, prefix);
+        if (key == null) return;
+        ProcessValues(key, prefix, results);
         foreach (var name in key.GetSubKeyNames())
         {
             try
             {
                 using RegistryKey? subKey = key.OpenSubKey(name);
-                if (subKey != null)
-                {
-                    string subPrefix = prefix + ConfigurationPath.KeyDelimiter + name;
-                    ProcessValues(subKey, subPrefix);
-                    ProcessKey(subKey, subPrefix);
-                }
+                if (subKey == null) continue;
+                string subPrefix = prefix + ConfigurationPath.KeyDelimiter + name;
+                ProcessValues(subKey, subPrefix, results);
+                ProcessKey(subKey, subPrefix, results);
             }
             catch { }
         }
@@ -55,20 +75,55 @@ internal class RegistryConfigurationProvider : ConfigurationProvider
 
     public override IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string? parentPath)
     {
-        return base.GetChildKeys(earlierKeys, parentPath);
-    }
-
-    public override void Load()
-    {
-        Data.Clear();
+        List<string> results = new(base.GetChildKeys(earlierKeys, parentPath));
         try
         {
             using RegistryKey root = RegistryKey.OpenBaseKey(source.RegistryHive, RegistryView.Default);
             using RegistryKey? key = root.OpenSubKey(source.Path);
-            if (key != null)
-                ProcessKey(key, prefix);
+            ProcessKey(key, prefix, results);
         }
         catch { }
+        bool added = (results.Count > 0);
+        results.AddRange(earlierKeys);
+        if (added) results.Sort(ConfigurationKeyComparer.Instance.Compare);
+        return results;
     }
 
+    public override bool TryGet(string key, out string? value) => (base.TryGet(key, out value) || TryGetRegistry(key, out value));
+
+    private bool TryGetRegistry(string key, out string? value)
+    {
+        value = null;
+        try
+        {
+            string[] parts = key.Split(ConfigurationPath.KeyDelimiter);
+            if (parts.Length < 2) return false;
+            if (parts[0] != prefix) return false;
+            using RegistryKey root = RegistryKey.OpenBaseKey(source.RegistryHive, RegistryView.Default);
+            using RegistryKey? rootKey = root.OpenSubKey(source.Path);
+            if (rootKey == null) return false;
+            if (parts.Length == 2)
+            {
+                if (!CanHandle(rootKey.GetValueKind(parts[^1]))) return false;
+                object? v = rootKey.GetValue(parts[^1]);
+                if (v == null) return false;
+                value = v.ToString();
+                return true; 
+            }
+            else
+            {
+                using RegistryKey? registryKey = root.OpenSubKey(String.Join('\\', parts[1..^1]));
+                if (registryKey == null) return false;
+                if (!CanHandle(registryKey.GetValueKind(parts[^1]))) return false;
+                object? v2 = registryKey.GetValue(parts[^1]);
+                if (v2 == null) return false;
+                value = v2.ToString();
+                return true;
+            }
+        }
+        catch { }
+        return false;
+    }
+
+    public override void Load() => Data.Clear();
 }
